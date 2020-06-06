7870de0861ed030a6495d9935c8092a903eb0613
Lucas Souza <lucas.b.souza@gmail.com>
2020-06-06 23:04:15 +0200

    Finish module 1
    Finished module 1 of the course, adding a simple behavior to the car like acelerating and rotating

---
+++ Prototyp1 - Bookish Guacamole/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get player input
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the car forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Rotate the car
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
